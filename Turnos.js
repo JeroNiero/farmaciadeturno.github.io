d8cff7fcfa687013543158a07045f9e3b6870042
Eric <eric@example.com>
2022-06-28 17:58:44 -0300

    cambios de Eric

--- Turnos.js
+++ Turnos.js
@@ -1,4 +1,8 @@
-
+const farmacias = {
+  "7 lagos" : [1, 3, 5, 7, 11, 13, 15, 19, 21, 23, 25, 27, 29, 31],
+  "Montaña" : [2, 4, 8, 10, 12, 16, 18, 20, 22, 24, 28, 30],
+  "Rios" : [6, 9, 14, 17, 26],
+}
 function diasDeTurno(farmacia) {
     if(farmacia  == "7 lagos") {
       return [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31];
@@ -19,7 +23,17 @@ function diasDeTurno(farmacia) {
     }
     
   }
-  
+
+  function farmaciasDeTurno(dia) {
+    let nombreFarmacia = "";
+    for (const farmacia in farmacias) {
+      var diasPorFarmacia = farmacias[farmacia];
+      if (diasPorFarmacia.includes(dia)){
+        nombreFarmacia = farmacia
+      }
+    }
+    return nombreFarmacia
+  }
   
   const result7Lagos = estaDeTurno("7 lagos", 1);
   console.log(result7Lagos); // standard output
