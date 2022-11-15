7d577720968f4c45c6f21ada3f26307a0291b1c1
Ericccc07 <ericletzen3@gmail.com>
2022-11-15 17:20:44 -0300

    Arreglados un par de bugs

--- dias_de_turno.js
+++ dias_de_turno.js
@@ -1,6 +1,6 @@
 const listaFarmacias = [
     {
-        nombre: 'Maiten',
+        nombre: ['Maiten'],
         fechaDeReferencia: '2022-05-01',
         ubicaciones:[
             'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3021.98464594031!2d-71.64820617997582!3d-40.76236243347259!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9610bf42d7c5a475%3A0x35484cf046254d4c!2sFarmacia%20Maiten!5e0!3m2!1ses!2sar!4v1660081489749!5m2!1ses!2sar'
@@ -10,7 +10,7 @@ const listaFarmacias = [
         ]
     },
     {
-        nombre: 'Pharmacy Siete Lagos',
+        nombre: ['Pharmacy Siete Lagos'],
         fechaDeReferencia: '2022-05-04',
         ubicaciones:[
             'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.263795803231!2d-71.65485365334109!3d-40.756222384548934!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x938fa8392d2c1ac5!2sPharmacy%207%20Lagos!5e0!3m2!1ses!2sar!4v1660081342553!5m2!1ses!2sar'
@@ -20,7 +20,7 @@ const listaFarmacias = [
         ]
     },
     {
-        nombre: 'Del Siglo' , 
+        nombre: ['Del Siglo'], 
         fechaDeReferencia: '2022-05-05',
         ubicaciones:[
             'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d377.7475545439012!2d-71.64279293021087!3d-40.76245501976972!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9610bf0c7fb3826d%3A0x5f7b4a06fcc0118e!2sFarmacia%20Del%20Siglo!5e0!3m2!1ses!2sar!4v1660080984836!5m2!1ses!2sar'    ],
@@ -29,7 +29,7 @@ const listaFarmacias = [
         ]
     },
     {
-        nombre: 'Pharmacy Avenida',
+        nombre: ['Pharmacy Avenida'],
         fechaDeReferencia: '2022-05-06',
         ubicaciones: [
             'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d755.4948855830874!2d-71.64274417079703!3d-40.76247468292443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x12d69b7536b7609!2sPharmacy%20Avenida!5e0!3m2!1ses!2sar!4v1667506694351!5m2!1ses!2sar', 
@@ -54,7 +54,7 @@ const listaFarmacias = [
         ]
     },
     {
-        nombre: 'Radal' , 
+        nombre: ['Radal'] , 
         fechaDeReferencia: '2022-05-08',
         ubicaciones:[
             'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3021.98464594031!2d-71.64820617997582!3d-40.76236243347259!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9610bf42d7c5a475%3A0x35484cf046254d4c!2sFarmacia%20Maiten!5e0!3m2!1ses!2sar!4v1660081489749!5m2!1ses!2sar'
@@ -64,7 +64,7 @@ const listaFarmacias = [
         ]
     },
     {
-        nombre: 'La Montaña' , 
+        nombre: ['La Montaña'] , 
         fechaDeReferencia: '2022-05-09',
         ubicaciones:[
             'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3021.9997415914604!2d-71.64563328482632!3d-40.76203041616233!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9610bf681dbaede5%3A0x5a9dddbe0000614d!2sFarmacia%20La%20Monta%C3%B1a!5e0!3m2!1ses!2sar!4v1667507297677!5m2!1ses!2sar'
